a8bf9fd2ad77728046c6586acb96fc55e79ec2ee
Edgar <Edgar@AnotherFoxGuy.com>
2019-11-25 12:38:41 +0100

    :sparkles: More CC Week 2 features

--- Concurrency/Week 2/main.cpp
+++ Concurrency/Week 2/main.cpp
@@ -3,22 +3,26 @@
 #include <cmath>
 #include <vector>
 #include <thread>
+#include <atomic>
+#include <mutex>
 
 using namespace std;
 
 void CheckRange(int min, int max);
 
-int modulus__;
+int modulus__, Kaas, Koekjes;
+
+mutex mtx;
+std::vector<std::thread> threadPool = {};
 
 int main() {
-    int b, e, m, p, u, s, n;
+    int b, e, m, p, u, s;
 
-    cout << "Type a number: " << endl;
-    scanf("%d %d %d %d %d %d", &b, &e, &m, &p, &u, &s);
+    cout << "Type Data ";
+    scanf("%*d %d %d %d %d %d %d", &b, &e, &m, &p, &u, &s);
 
     modulus__ = m;
-
-    std::vector<std::thread> threadPool = {};
+    Koekjes = u;
 
     int range = e - b;
     int rk = range / p;
@@ -32,28 +36,33 @@ int main() {
         thread.join();
     }
 
+    if (u == 0)
+        printf("%d", Kaas);
+
+}
+
+void QuitThreads() {
+    for (auto &thread : threadPool) {
+        thread.~thread();
+    }
 }
 
 
 void CheckRange(int min, int max) {
-    try {
-        for (int kjrsdgfnhgdsf = min; kjrsdgfnhgdsf < max; ++kjrsdgfnhgdsf) {
-            int iBan = kjrsdgfnhgdsf;
-            int iBanLength = log10(iBan) + 1;
-            int checkSum = 0;
-            for (int i = 1; i <= iBanLength; ++i) {
-                checkSum += ((iBan % 10) * i);
-                iBan /= 10;
-            }
+    for (int kjrsdgfnhgdsf = min; kjrsdgfnhgdsf < max; ++kjrsdgfnhgdsf) {
+        int iBan = kjrsdgfnhgdsf;
+        int iBanLength = log10(iBan) + 1;
+        int checkSum = 0;
+        for (int i = 1; i <= iBanLength; ++i) {
+            checkSum += ((iBan % 10) * i);
+            iBan /= 10;
+        }
 
-            if (checkSum % modulus__ == 0) {
-                cout << kjrsdgfnhgdsf << endl;
-            }
+        if (checkSum % modulus__ == 0) {
+            std::scoped_lock lock(mtx);
+            Kaas++;
+            if (Koekjes != 0)
+                printf("%d %d\n", Kaas, kjrsdgfnhgdsf);
         }
     }
-    catch (exception &e) {
-        cout << e.what() << endl;
-    }
-
-
 }
